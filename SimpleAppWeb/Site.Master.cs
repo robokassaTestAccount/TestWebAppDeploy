e48819f89c190a37308a5a72abcc21dbe7c2c243
r <iutrobin@IUTROBIN>
2018-09-20 16:36:19 +0300

    test1

--- SimpleAppWeb/Site.Master.cs
+++ SimpleAppWeb/Site.Master.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace SimpleAppWeb
 {
@@ -12,6 +9,20 @@ namespace SimpleAppWeb
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string TextToDB = TextBox1.Text;
+            using (var context = new TestDataBase_DontTouchEntities())
+            {
+                var NewLine = new TestTable
+                {
+                    text1 = TextToDB
+                };
+                context.TestTable.Add(NewLine);
+                context.SaveChanges();
+            }
         }
     }
 }
